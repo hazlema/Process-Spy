81174194027fe4e3dbc7be49157f7bfbe1de8d2a
hazlema <hazlema@gmail.com>
2016-06-08 21:38:28 -0400

    oops, now final

--- Process Spy/Form1.Designer.cs
+++ Process Spy/Form1.Designer.cs
@@ -53,7 +53,7 @@
             this.tree.Location = new System.Drawing.Point(0, 25);
             this.tree.Margin = new System.Windows.Forms.Padding(12, 10, 12, 10);
             this.tree.Name = "tree";
-            this.tree.Size = new System.Drawing.Size(280, 222);
+            this.tree.Size = new System.Drawing.Size(242, 222);
             this.tree.TabIndex = 0;
             this.tree.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tree_NodeMouseClick);
             // 
@@ -85,7 +85,7 @@
             this.btnRefresh});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(280, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(242, 25);
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -107,7 +107,7 @@
             this.statusStrip1.Location = new System.Drawing.Point(0, 247);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 16, 0);
-            this.statusStrip1.Size = new System.Drawing.Size(280, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(242, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -131,12 +131,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(280, 269);
+            this.ClientSize = new System.Drawing.Size(242, 269);
             this.Controls.Add(this.tree);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.toolStrip1);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
